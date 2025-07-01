120c1f72f6f0eeddb1fa0b0cdab343548dc06d19
김혜주 <50792699+bolisnsk@users.noreply.github.com>
2025-07-01 16:41:28 +0900

    사진 추가 및 앤톤 명언 추가

--- mstyle.js
+++ mstyle.js
@@ -5,12 +5,27 @@ textElements.forEach(text => {
     ease: 'none',
     scrollTrigger: {
       trigger: text,
-      start: 'center 70%',
+      start: 'center 80%',
       end: 'center 50%',
       scrub: true,
     },
   });
 });
+
+const textElements2 = gsap.utils.toArray('#inc01 .h_txt');
+textElements2.forEach(text => {
+  gsap.to(text, {
+    backgroundSize: '100%',
+    ease: 'none',
+    scrollTrigger: {
+      trigger: text,
+      start: 'center 80%',
+      end: 'center 50%',
+      scrub: true,
+    },
+  });
+});
+
 window.onload = () => {
     let x = 0,
         y = 0;
@@ -49,4 +64,23 @@ window.onload = () => {
         
     };
 
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY;
+      const textBox = document.getElementById('scrollText');
+      const imageBox = document.getElementById('scrollImage');
 
+      // 텍스트가 보이도록
+      if (scrollY > 100 && scrollY < 600) {
+        textBox.classList.add('visible');
+      } else {
+        textBox.classList.remove('visible');
+      }
+
+      // 이미지가 보이도록, 텍스트는 사라지도록
+      if (scrollY > 700) {
+        imageBox.classList.add('visible');
+        textBox.classList.remove('visible');
+      } else {
+        imageBox.classList.remove('visible');
+      }
+    });
